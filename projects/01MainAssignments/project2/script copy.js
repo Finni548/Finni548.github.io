185ca0cbb31fecc258a4cf3210be10d243ca5582
Finni548 <finnja.kraemer@zhdk.ch>
2025-11-25 09:39:47 +0100

    change speaker

--- projects/01MainAssignments/project2/script copy.js
+++ projects/01MainAssignments/project2/script copy.js
@@ -5,6 +5,7 @@ let handleX;
 
 function setup() {
   createCanvas(400, 300);
+	// windowWidth, windowHeight
   sliderX = 100;
   handleX = sliderX + sliderW/2;
 }
@@ -12,17 +13,43 @@ function setup() {
 function draw() {
   background(240);
 
-  // linie
+  drawSpeakerIcon(sliderX - 40, sliderY, 1);
+
+  drawSpeakerIcon(sliderX + sliderW + 40, sliderY, 2);
+
+  //Linie
   stroke(0);
   line(sliderX, sliderY, sliderX + sliderW, sliderY);
 
-  // handleX
+  //handleX
   fill(200, 50, 150);
+  noStroke();
   ellipse(handleX, sliderY, 20, 20);
 
-  // Bewegt sich weg
+  // Bewegt sich weg 
   if (dist(mouseX, mouseY, handleX, sliderY) < 50) {
-    handleX = sliderX + random(sliderW); 
+    handleX = sliderX + random(sliderW);
+  }
+}
+
+// Einfache Lautsprecher 
+function drawSpeakerIcon(x, y, waves) {
+  stroke(0);
+  strokeWeight(2);
+
+  // Speaker box
+  line(x - 10, y - 10, x - 10, y + 10);
+  line(x - 10, y - 10, x, y - 5);
+  line(x - 10, y + 10, x, y + 5);
+  line(x, y - 5, x, y + 5);
+
+  // Waves
+  if (waves >= 1) {
+    noFill();
+    arc(x + 5, y, 10, 10, -0.5, 0.5);
+  }
+  if (waves >= 2) {
+    arc(x + 10, y, 20, 20, -0.5, 0.5);
   }
 }
 
