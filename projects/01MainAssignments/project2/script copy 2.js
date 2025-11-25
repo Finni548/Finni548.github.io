d02ac598760f682d02f22184dc71c27e6078c55b
Finni548 <finnja.kraemer@zhdk.ch>
2025-11-25 16:16:42 +0100

    change catch me if you can

--- projects/01MainAssignments/project2/script copy 2.js
+++ projects/01MainAssignments/project2/script copy 2.js
@@ -1,36 +1,68 @@
-let circleX;
-let circleY;
+let circleX = [];
+let circleY = [];
 let circleSize = 50;
+let numCircles = 5;
+
+let showMessage = false;   
+let messageTimer = 0;      
+let messageText = "";      
+
+let attempts = [];          
+let active = [];            
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  // Startposition des Kreises
-  circleX = width / 2;
-  circleY = height / 2;
+  // Start
+  for (let i = 0; i < numCircles; i++) {
+    circleX[i] = random(circleSize / 2, width - circleSize / 2);
+    circleY[i] = random(circleSize / 2, height - circleSize / 2);
+    attempts[i] = 0;       
+    active[i] = true;      
+  }
 }
 
 function draw() {
   background(255, 209, 220);
 
-	// Titel oben mittig
-  fill(0);
-  textSize(40);
+  
+  fill(255, 0, 150);
+  textSize(50);
   textAlign(CENTER, TOP);
   text("Catch me if you can", width / 2, 20);
 
-  // Kreis zeichnen
   fill(100, 0, 200);
   noStroke();
-  ellipse(circleX, circleY, circleSize);
 
-  // Abstand zur Maus
-  let d = dist(mouseX, mouseY, circleX, circleY);
+  for (let i = 0; i < numCircles; i++) {
+    if (!active[i]) continue; 
 
-  // Wenn Maus zu nah kommt → Kreis springt weg
-  if (d < circleSize) {
-    circleX = random(circleSize/2, width - circleSize/2);
-    circleY = random(circleSize/2, height - circleSize/2);
+    ellipse(circleX[i], circleY[i], circleSize);
+
+    let d = dist(mouseX, mouseY, circleX[i], circleY[i]);
+
+    if (d < circleSize) {
+      attempts[i]++;  
+      messageTimer = millis();
+      showMessage = true;
+
+      if (attempts[i] >= 5) {
+        active[i] = false;   
+        messageText = "Yes, perfect!";
+      } else {
+        
+        circleX[i] = random(circleSize / 2, width - circleSize / 2);
+        circleY[i] = random(circleSize / 2, height - circleSize / 2);
+        messageText = "next time";
+      }
+    }
+  }
+
+  if (showMessage && millis() - messageTimer < 1000) {
+    fill(0);
+    textSize(30);
+    textAlign(CENTER, BOTTOM);
+    text(messageText, width / 2, height - 20);
   }
 }
 
