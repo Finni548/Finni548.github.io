09a2943936344970da8832b220923d20113d1f08
Finni548 <finnja.kraemer@zhdk.ch>
2025-11-25 22:18:00 +0100

    bubbles

--- projects/01MainAssignments/project2/script copy 2.js
+++ projects/01MainAssignments/project2/script copy 2.js
@@ -13,44 +13,40 @@ let active = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
   
-  // Start
   for (let i = 0; i < numCircles; i++) {
     circleX[i] = random(circleSize / 2, width - circleSize / 2);
     circleY[i] = random(circleSize / 2, height - circleSize / 2);
-    attempts[i] = 0;       
-    active[i] = true;      
+    attempts[i] = 0;
+    active[i] = true;
   }
 }
 
 function draw() {
   background(255, 209, 220);
 
-  
   fill(255, 0, 150);
   textSize(50);
   textAlign(CENTER, TOP);
   text("Catch me if you can", width / 2, 20);
 
-  fill(100, 0, 200);
   noStroke();
 
   for (let i = 0; i < numCircles; i++) {
-    if (!active[i]) continue; 
+    if (!active[i]) continue;
 
-    ellipse(circleX[i], circleY[i], circleSize);
+    drawBubble(circleX[i], circleY[i], circleSize);
 
     let d = dist(mouseX, mouseY, circleX[i], circleY[i]);
 
     if (d < circleSize) {
-      attempts[i]++;  
+      attempts[i]++;
       messageTimer = millis();
       showMessage = true;
 
       if (attempts[i] >= 5) {
-        active[i] = false;  
+        active[i] = false;
         messageText = "Yes, perfect!";
       } else {
-        
         circleX[i] = random(circleSize / 2, width - circleSize / 2);
         circleY[i] = random(circleSize / 2, height - circleSize / 2);
         messageText = "next time";
@@ -64,5 +60,26 @@ function draw() {
     textAlign(CENTER, BOTTOM);
     text(messageText, width / 2, height - 20);
   }
+}
+
+function drawBubble(x, y, s) {
+  // Äußere Schicht
+  fill(255, 255, 255, 40);
+  ellipse(x, y, s * 1.15);
+
+  // Hauptkörper
+  fill(180, 200, 255, 120);
+  ellipse(x, y, s);
+
+  // Schimmer
+  fill(255, 255, 255, 180);
+  ellipse(x - s * 0.2, y - s * 0.2, s * 0.35);
+
+  // zarter Rand
+  noFill();
+  stroke(255, 255, 255, 150);
+  strokeWeight(1);
+  ellipse(x, y, s);
+  noStroke();
 }
 
